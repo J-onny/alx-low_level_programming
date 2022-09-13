4cecaa98697454e13f4771b9bf75a5da4dd09cea
j-onny <omo.pro22@gmail.com>
2022-09-13 15:59:43 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number p
+ */
+int _abs(int p)
+{
+	if (p >= 0)
+		return (p);
+	else
+		return (p * -1);
+}

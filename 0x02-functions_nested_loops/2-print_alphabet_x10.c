bfbf9ae292d6cf0832a88d1f666cc53ac61ac21e
j-onny <omo.pro22@gmail.com>
2022-09-13 15:12:35 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**Na _Check The Code
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char n;
+	char q;
+
+	for(n=1;n<=10;n++)
+	{
+		for(q='a';q<='z';q++)
+		{
+			_putchar('q');
+			_putchar('\n');
+		}
+/*8		return(0);*/	
+	}
+}

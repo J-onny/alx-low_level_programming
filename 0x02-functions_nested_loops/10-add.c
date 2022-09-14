09f014a60f04e7c0c7457a3f9e5e1ac0198c991c
j-onny <omo.pro22@gmail.com>
2022-09-13 21:43:57 -0700

    c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,18 @@
 /**
  *Add integers - Check description
  * adds two integers and returns the result.
- * Return: Nothing.
+ * Return: Nothing
+ * x is the firstinteger variable to be added
+ * y is the second integer vartiable to be added
+ * the add function provides the sum of x and y
  */
 int add(int x, int y)
+
 {
 	int result;
+
 	result = x + y;
-	return(result);
+
+	return (result);
 
 }

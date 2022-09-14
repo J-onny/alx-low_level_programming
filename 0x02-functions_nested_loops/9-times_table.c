e36d3fafc39930dc31f9d63892271b0884713dd6
j-onny <omo.pro22@gmail.com>
2022-09-13 21:20:58 -0700

    table correct

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,17 @@
  */
 void times_table(void)
 { int i, j, pdt;
-	for (i=0; i<=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j=0;j<=9;j++)
+		for (j = 0; j <= 9 ; j++)
 		{
-			pdt=j*i;
+			pdt = j * i;
 
-			if(j==0)
+			if (j == 0)
 			{
 				_putchar(pdt + '0');
 			}
-			if(pdt<=0 && j!=0)
+			if (pdt <= 0 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -25,13 +25,13 @@ void times_table(void)
 				_putchar(pdt + '0');
 			}
 
-			else if(pdt>+0)
+			else if (pdt >= 0)
 			{
-				_putchar(',');
-                                _putchar(' ');
-                                _putchar(' ');
-				_putchar((pdt/10) + '0');
-				_putchar((pdt%10) +'0');
+			_putchar(',');
+			 _putchar(' ');
+			 _putchar(' ');
+			_putchar((pdt / 10) + '0');
+			_putchar((pdt % 10) + '0');
 
 			}
 			_putchar('\n');

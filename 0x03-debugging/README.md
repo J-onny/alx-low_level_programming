f38a3ba2df6e31fbb3439c71c95d7eb320104118
j-onny <omo.pro22@gmail.com>
2022-09-16 19:41:18 -0700

    more functions and loops

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+s one is about Debugging
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ * @a: An input integer
+ * @b: An input integer
+ *@pdt: Profuct of the two integers
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+    int pdt;
+    pdt = a * b;
+	return (pdt);
+}
+

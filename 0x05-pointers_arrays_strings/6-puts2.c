1d6cae04e91d4aba9f99384d59e74aece36cefdf
j-onny <omo.pro22@gmail.com>
2022-09-19 16:20:54 -0700

    puts2&&Putshalf

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prints every other character of a string,
+ * puts2 -  prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: An input string
  * Return: Nothing

9abc55e19c0f9b4dc0a12fd95c02e5a2af20b65c
j-onny <omo.pro22@gmail.com>
2022-09-21 15:59:14 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,24 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * _strncat - Concatenates two strings.
- * @dest: an input string
- * @src: an input string
- * @n: an input integer
- * Return: A pointer to the resulting string
- 
-#include <string.h>
-:winclude "main.h"
-
-*/
-char *_strncpy(char *dest, char *src, int n)
+ * _strcat - concatenates two strings.
+ * @dest: the destination string
+ * @src: the source string
+ * Return: the concatenated string
+ */
+char *_strcat(char *dest, char *src)
 {
-	int i, lenDest;
+	int i, lenDest, lenSrc;
 
 	lenDest = strlen(dest);
+	lenSrc = strlen(src);
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < lenSrc; i++)
 	{
-		if (n < lenDest)
-			dest[i] = src[i];
+		dest[lenDest + i] = src[i];
 	}
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
+	dest[lenDest + i] = '\0';
 	return (dest);
 }
 

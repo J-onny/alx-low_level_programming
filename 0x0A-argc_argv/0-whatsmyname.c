f8f5e7fd12b8198b634e94c557bd0792cc140f4c
j-onny <omo.pro22@gmail.com>
2022-10-04 17:18:49 -0700

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,15 @@
-#include "main.h"
-/**
- *@argc count of the variable passed alon with the command line
+#include "main"
 
- *@argv the character variable passes to the argument
- *
- *
- *
- * */
-int main(int argc , char *argv[])
+/**
+ * main - prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument of, a strings that come after calling function
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-	
 		printf("%s\n", argv[0]);
-	return 0;
-}
+
+	return (0);
 
